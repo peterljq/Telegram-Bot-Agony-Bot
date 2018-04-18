dacffc225a707ed1c0af0482e5ac160bacff90ce
Luo J.Q <37497646+peterljq@users.noreply.github.com>
2018-04-18 17:10:23 +0800

    Update publish.py

--- publish.py
+++ publish.py
@@ -18,7 +18,7 @@ def publish(announcement):
         conn.close()
 
 a= "Hello guys!  How is everything going? I am your Agony Aunt!\nToday is Friday, our weekly ranking announcing day! Here I am proud to tell you the top ten liked thoughts during the past week!"
-b= "The gold medal winner:\n'帅哥Adrian can decode more than python. Wanna try him? ;)'\n\nThe first runner-up:\n'College is a little overwhelming... anyone feel the same way'\n\nThe third place thought:\n'Like if you like Tori Black'\n\nThen follows the 4-10th most liked thoughts:\n'sia！interesting bot！'\n'Life's tough but what's life without some hiccups'\n'Hi nice to meet you ❤️❤️❤️❤️❤️❤️'\n'ENROLLING IN COMPUTER SCIENCE IS THE BEST DECISION I HAVE EVER MADE!!!❤️❤️❤️'\n'How do I feel less bad about being single? :('\n'Watching wk 5 lectures now..'\n'I'm hungry can I eat someone'"
+b= "The gold medal winner:\n'Adrian can decode more than python. Wanna try him? ;)'\n\nThe first runner-up:\n'College is a little overwhelming... anyone feel the same way'\n\nThe third place thought:\n'Like if you like Tori Black'\n\nThen follows the 4-10th most liked thoughts:\n'sia！interesting bot！'\n'Life's tough but what's life without some hiccups'\n'Hi nice to meet you ❤️❤️❤️❤️❤️❤️'\n'ENROLLING IN COMPUTER SCIENCE IS THE BEST DECISION I HAVE EVER MADE!!!❤️❤️❤️'\n'How do I feel less bad about being single? :('\n'Watching wk 5 lectures now..'\n'I'm hungry can I eat someone'"
 c= "Thank you guys for sharing amazing thoughts! \nCome on guys, let’s share and reply!  Little little thoughts, big big network!"
 
 
